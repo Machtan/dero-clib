ede763cf0ead44910b7a23d8ac685f771bff54ba
Machtan <jako3047@gmail.com>
2016-09-08 22:51:29 +0200

    Make things more conventional

--- dero.h
+++ dero.h
@@ -1,16 +1,16 @@
 #include <stdint.h>
 
-typedef int32_t dero_Status;
+typedef int32_t dero_status_t;
 
-const dero_Status dero_OK = 0;
-const dero_Status dero_ERR_NULL = -1;
-const dero_Status dero_ERR_UTF8 = -2;
-const dero_Status dero_ERR_CONVERT = -3;
-const dero_Status dero_ERR_PANIC = -128;
+const dero_status_t DERO_OK = 0;
+const dero_status_t DERO_ERR_NULL = -1;
+const dero_status_t DERO_ERR_UTF8 = -2;
+const dero_status_t DERO_ERR_CONVERT = -3;
+const dero_status_t DERO_ERR_PANIC = -128;
 
-const char *dero_error_message(dero_Status);
+const char *dero_error_message(dero_status_t);
 
-dero_Status dero_convert(const char *text, const char **output);
+dero_status_t dero_convert(const char *text, const char **output);
 
 void dero_free_converted(const char *);
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![allow(non_upper_case_globals)]
 extern crate dero;
 extern crate libc;
 
@@ -9,20 +8,20 @@ use std::io::{self, Write};
 use std::panic;
 use std::process;
 
-pub const dero_OK: i32 = 0;
-pub const dero_ERR_NULL: i32 = -1;
-pub const dero_ERR_UTF8: i32 = -2;
-pub const dero_ERR_CONVERT: i32 = -3;
-pub const dero_ERR_PANIC: i32 = -128;
+pub const DERO_OK: i32 = 0;
+pub const DERO_ERR_NULL: i32 = -1;
+pub const DERO_ERR_UTF8: i32 = -2;
+pub const DERO_ERR_CONVERT: i32 = -3;
+pub const DERO_ERR_PANIC: i32 = -128;
 
 #[no_mangle]
 pub unsafe extern "C" fn dero_error_message(error_code: i32) -> *const c_char {
     let bytes: &'static [u8] = match error_code {
-        dero_OK => &b"no error\0"[..],
-        dero_ERR_NULL => &b"the text pointer is null\0"[..],
-        dero_ERR_UTF8 => &b"invalid UTF-8 in text\0"[..],
-        dero_ERR_CONVERT => &b"the full text could not be converted\0"[..],
-        dero_ERR_PANIC => &b"the converter panicked\0"[..],
+        DERO_OK => &b"no error\0"[..],
+        DERO_ERR_NULL => &b"the text pointer is null\0"[..],
+        DERO_ERR_UTF8 => &b"invalid UTF-8 in text\0"[..],
+        DERO_ERR_CONVERT => &b"the full text could not be converted\0"[..],
+        DERO_ERR_PANIC => &b"the converter panicked\0"[..],
         _ => &b"unknown error\0"[..],
     };
     bytes.as_ptr() as *const c_char
@@ -60,28 +59,28 @@ pub unsafe extern "C" fn dero_explain_error(text: *const c_char) {
 pub unsafe extern "C" fn dero_convert(text: *const c_char, output: *mut *const c_char) -> i32 {
     let res = panic::catch_unwind(move || {
         if text.is_null() {
-            return dero_ERR_NULL;
+            return DERO_ERR_NULL;
         }    
         let res = str::from_utf8(CStr::from_ptr(text).to_bytes());
         let text = if let Ok(text) = res {
             text
         } else {
-            return dero_ERR_UTF8;
+            return DERO_ERR_UTF8;
         };
         match dero::convert(text) {
             Ok(converted) => {
                 let cstring = if let Ok(cstring) = CString::new(converted) {
                     cstring
                 } else {
-                    return dero_ERR_UTF8;
+                    return DERO_ERR_UTF8;
                 };
                 *output = cstring.into_raw();
-                dero_OK
+                DERO_OK
             }
-            Err(_) => dero_ERR_CONVERT,
+            Err(_) => DERO_ERR_CONVERT,
         }
     });
-    res.unwrap_or(dero_ERR_PANIC)
+    res.unwrap_or(DERO_ERR_PANIC)
 }
 
 #[no_mangle]
